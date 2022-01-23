aabe8c834a2748f1f4efa256cdfe3ec53f145a5a
VaibhavPaliwal0007 <78612380+VaibhavPaliwal0007@users.noreply.github.com>
2022-01-24 03:56:24 +0530

    dynamically creating elements

--- salary-calc/scripts/controller.js
+++ salary-calc/scripts/controller.js
@@ -5,12 +5,25 @@ function bindEvents() {
 }
 function computeIt() {
   allowances.basicSalary = parseInt(document.getElementById("basicsalary").value);
+  let fieldset = document.querySelector('fieldset');
 
-  document.getElementById("hra").innerText = allowances.hra();
-  document.getElementById("da").innerText = allowances.da();
-  document.getElementById("ta").innerText = allowances.ta();
-  document.getElementById("ma").innerText = allowances.ma();
-  document.getElementById("pf").innerText = allowances.pf();
-  document.getElementById("gs").innerText = allowances.gs();
-  document.getElementById("ns").innerText = allowances.ns();
+  fieldset.innerHTML = "";
+
+  for(const func in allowances){   //key
+     if(typeof allowances[func] === 'function'){
+        let p = document.createElement('p');
+
+        p.innerHTML = func + ':   ' + `<span>&#8377 ${allowances[func]().toLocaleString('hi-IN')}</span>`;
+
+        fieldset.appendChild(p);
+     }
+  }
+
+  // document.getElementById("hra").innerText = allowances.hra();
+  // document.getElementById("da").innerText = allowances.da();
+  // document.getElementById("ta").innerText = allowances.ta();
+  // document.getElementById("ma").innerText = allowances.ma();
+  // document.getElementById("pf").innerText = allowances.pf();
+  // document.getElementById("gs").innerText = allowances.gs();
+  // document.getElementById("ns").innerText = allowances.ns();
 }
